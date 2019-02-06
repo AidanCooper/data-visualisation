297f3d89ee6df2132ce9bdb9ff0f9b572a4f111f
AidanCooper <aidan@aidancooper.co.uk>
2019-02-06 01:04:10 +0000

    Create basic dashboard
    Simple dashboard for London cycle hire data

--- dash/app.py
+++ dash/app.py
@@ -2,6 +2,11 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+
+df = pd.read_csv('cycle-hires-prepared.csv', index_col=0, parse_dates=True)
+df_d = df.groupby(df.index.dayofweek).mean()
+df_m = df.groupby(df.index.month).mean()
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -14,18 +19,64 @@ app.layout = html.Div(children=[
         Dash: A web application framework for Python.
     '''),
 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
+    html.Div(
+        dcc.Graph(
+            id='timeseries-graph',
+            figure={
+                'data': [
+                    {'x': df.index, 'y': df['Number of Bicycle Hires']},
+                ],
+                'layout': {
+                    'title': 'Bicycle hires over time'
+                }
             }
-        }
-    )
+        )
+    ),
+
+    html.Div(children=[
+        html.Div(
+            dcc.Graph(
+                id='histogram-graph',
+                figure={
+                    'data': [
+                        {'x': df['Number of Bicycle Hires'],
+                         'type': 'histogram'},
+                    ],
+                    'layout': {
+                        'title': 'Number of daily bicycle hires histogram'
+                    }
+                }
+            ), style={'width': '33%', 'display': 'inline-block'}
+        ),
+        html.Div(
+            dcc.Graph(
+                id='day-graph',
+                figure={
+                    'data': [
+                        {'x': df_d.index, 'y': df_d['Number of Bicycle Hires'],
+                         'type': 'bar'},
+                    ],
+                    'layout': {
+                        'title': 'Daily bicycle hires by day of week'
+                    }
+                }
+            ), style={'width': '33%', 'display': 'inline-block'}
+        ),
+        html.Div(
+            dcc.Graph(
+                id='month-graph',
+                figure={
+                    'data': [
+                        {'x': df_m.index, 'y': df_m['Number of Bicycle Hires'],
+                         'type': 'bar'},
+                    ],
+                    'layout': {
+                        'title': 'Daily bicycle hires by month'
+                    }
+                }
+            ), style={'width': '33%', 'display': 'inline-block'}
+        )
+    ])
 ])
 
 
